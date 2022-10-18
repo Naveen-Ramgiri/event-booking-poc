2086a2042b3fe6233d079020fe7465c580c90d1a
Kumar Shoolam <83000460+kumarshoolam2008@users.noreply.github.com>
2022-10-18 14:58:36 +0530

    Linked ReactCalendar to the other options.

--- src/Components/CreateEvents/CreateEvent.js
+++ src/Components/CreateEvents/CreateEvent.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import '../CreateEvents/CreateEvent.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "../CreateEvents/CreateEvent.css";
+import { Link } from "react-router-dom";
+import ReactCalendar from "../Calendar/ReactCalendar";
 
 function CreateEvent() {
-    return (
-        <div className='create__event'>
-            <div className='container'>
-                <div className='create__event-steps'>
-                    <div>
-                        <h4>What type of event is it?</h4>
-                        <p>Already have an account? <Link to="/Login">LOG IN </Link>or LOOK UP EVENT »</p>
-                        <ul>
-                            <li>
-                                <Link to="/EventType">
-                                    <h5>Wedding Party</h5>
-                                    <i class="fa fa-life-ring" aria-hidden="true"></i>
-                                </Link>
-
-                            </li>
-                            <li>
-                                <Link to="">
-                                    <h5>HomeComing/Fall formal</h5>
-                                    <i class='fas fa-crown'></i>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/MardiGras">
-                                    <h5>FOrmal Event - Single Rental</h5>
-                                    <i class="fa fa-user-o"></i>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/MardiGras">
-                                    <h5>MARDI GRAS</h5>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="create__event">
+      <div className="container">
+        <div className="create__event-steps">
+          <div>
+            <h4>What type of event is it?</h4>
+            <p>
+              Already have an account? <Link to="/Login">LOG IN </Link>or LOOK
+              UP EVENT »
+            </p>
+            <ul>
+              <li>
+                <Link to="/EventType">
+                  <h5>Wedding Party</h5>
+                  <i class="fa fa-life-ring" aria-hidden="true"></i>
+                </Link>
+              </li>
+              <li>
+                <Link to="/ReactCalendar">
+                  <h5>HomeComing/Fall formal</h5>
+                  <i class="fas fa-crown"></i>
+                </Link>
+              </li>
+              <li>
+                <Link to="/MardiGras">
+                  <h5>FOrmal Event - Single Rental</h5>
+                  <i class="fa fa-user-o"></i>
+                </Link>
+              </li>
+              <li>
+                <Link to="/MardiGras">
+                  <h5>MARDI GRAS</h5>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default CreateEvent;
--- src/Components/CreateEvents/MardiGras.js
+++ src/Components/CreateEvents/MardiGras.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function MardiGras() {
-    return (
-        <div className='create__event'>
-            <div className='container'>
-                <div className='create__event-steps'>
-                    <div>
-                        <h4>Organizing for a group?</h4>
-                        <p>Create a group to manage and keep track of those attending.</p>
-                        <ul>
-                            <li>
-                                <Link to="">
-                                    <h5>YES,CREATE A GROUP</h5>                                   
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="">
-                                    <h5>NO,CREATE A SINGLE RENTAL</h5>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="create__event">
+      <div className="container">
+        <div className="create__event-steps">
+          <div>
+            <h4>Organizing for a group?</h4>
+            <p>Create a group to manage and keep track of those attending.</p>
+            <ul>
+              <li>
+                <Link to="/ReactCalendar">
+                  <h5>YES,CREATE A GROUP</h5>
+                </Link>
+              </li>
+              <li>
+                <Link to="/ReactCalendar">
+                  <h5>NO,CREATE A SINGLE RENTAL</h5>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default MardiGras
+export default MardiGras;
